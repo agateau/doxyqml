7b37f6431ccd030f0d870032d797ddb6d33ae7b7
Sanjay Joshi <smjoshi@globusmedical.com>
2018-05-11 11:23:10 -0400

    Updated test cases to test import in a comment and in variable names

--- tests/functional/relative-import/expected/RelativeImport.qml.cpp
+++ tests/functional/relative-import/expected/RelativeImport.qml.cpp
@@ -1,8 +1,11 @@
 using namespace QtQuick;
 using namespace QtQuick::Controls;
 using namespace QtQuick::Controls::Styles;
+// import in a comment
 class RelativeImport : public QtObject {
 public:
 
-Q_PROPERTY(string hello)
+Q_PROPERTY(string import_hello)
+
+Q_PROPERTY(string hello_import)
 };
--- tests/functional/relative-import/input/RelativeImport.qml
+++ tests/functional/relative-import/input/RelativeImport.qml
@@ -3,7 +3,9 @@ import QtQuick.Controls 1.0
  import QtQuick.Controls.Styles 1.0
 import  "."
 import "qrc:///something"
+// import in a comment
 
 QtObject {
-    property string hello: "world"
+    property string import_hello: "world"
+    property string hello_import: "import"
 }
