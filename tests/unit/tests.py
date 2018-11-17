6ca0cdf1e13c1854d6e0da9ce464b9489a350d9c
Aurelien Gateau <mail@agateau.com>
2018-11-18 00:25:05 +0100

    Use test discover feature

--- tests/unit/tests.py
+++ tests/unit/tests.py
@@ -1,19 +1,11 @@
 #!/usr/bin/env python3
-
 import os
-import unittest
 import sys
+import unittest
 
-doxyqml_path = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir)
-sys.path.insert(0, doxyqml_path)
-
-from qmlclasstestcase import *
-from qmlparsertestcase import *
-from lexertestcase import *
-
-def main():
-    unittest.main()
 
 if __name__ == "__main__":
-    main()
+    current_dir = os.path.dirname(__file__)
+    args = [sys.argv[0]] + ["discover", "--start-directory", current_dir, "--pattern", "*testcase.py"] + sys.argv[1:]
+    unittest.main(module=None, argv=args)
 # vi: ts=4 sw=4 et
