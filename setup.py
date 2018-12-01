11f9cfe03523e47653b23459492ac42390699f3a
Aurelien Gateau <mail@agateau.com>
2018-12-01 15:47:43 +0100

    Generate long_description from README.md

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
+import os
 
 from setuptools import setup
 
 from doxyqml import __version__, DESCRIPTION
 
+
+def read_readme():
+    root_dir = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(root_dir, 'README.md'), encoding='utf-8') as f:
+        return f.read()
+
+
 setup(name="doxyqml",
       version=__version__,
       description=DESCRIPTION,
+      long_description=read_readme(),
+      long_description_content_type="text/markdown",
       author="Aurélien Gâteau",
       author_email="mail@agateau.com",
       license="BSD",
